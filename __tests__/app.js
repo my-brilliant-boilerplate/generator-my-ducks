721bc58e596c4d74b506fc46b91df9155445568a
AlekseyLeshko <alekseyleshko@gmail.com>
2017-12-08 19:09:00 +0400

    fix tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,45 +3,43 @@ const assert = require('yeoman-assert');
 const utils = require('./helpers/utils.js');
 const helper = require('./helpers/index.js');
 
+const srcPath = 'testSrc';
+const reducers = ['servers', 'auth'];
+
+const expectedFiles = ['store/index', 'modules/rootReducer'];
+const reducerFiles = [
+  'actions',
+  'index',
+  'index.test',
+  'constants',
+  'selectors',
+  'selectors.test'
+];
+const files = expectedFiles.map(file => `${srcPath}/${file}.js`);
+
 describe('generator-my-ducks:app', () => {
-  const srcPath = 'testSrc';
-  const reducers = ['servers', 'auth'];
-
-  describe('should creates', () => {
-    const expectedFiles = ['store/index', 'modules/rootReducer'];
-    const reducerFiles = [
-      'actions',
-      'index',
-      'index.test',
-      'constants',
-      'selectors',
-      'selectors.test'
-    ];
-    const files = expectedFiles.map(file => `${srcPath}/${file}.js`);
-
-    it('all files', done => {
-      files.concat(utils.getFileForReducer(srcPath, reducers, reducerFiles));
-      helper.app({
-        prompts: { srcPath, reducers: reducers.join(',') },
-        done: () => {
-          assert.file(files);
-          done();
-        }
-      });
+  it('should creates all files', done => {
+    files.concat(utils.getFileForReducer(srcPath, reducers, reducerFiles));
+    helper.app({
+      prompts: { srcPath, reducers: reducers.join(',') },
+      done: () => {
+        assert.file(files);
+        done();
+      }
     });
+  });
 
-    it('files without reducers', done => {
-      const filesNotExists = utils.getFileForReducer(srcPath, reducers, reducerFiles);
+  it('should creates files without reducers', done => {
+    const filesNotExists = utils.getFileForReducer(srcPath, reducers, reducerFiles);
 
-      helper.app({
-        prompts: { srcPath, reducers: '' },
-        done: () => {
-          assert.file(files);
-          assert.noFile(filesNotExists);
+    helper.app({
+      prompts: { srcPath, reducers: '' },
+      done: () => {
+        assert.file(files);
+        assert.noFile(filesNotExists);
 
-          done();
-        }
-      });
+        done();
+      }
     });
   });
 });
